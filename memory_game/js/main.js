ceac8cf1df78f1e82a2e953a4ab859031f511329
Georg Preuss <geronimo@MacBook-Pro.local>
2019-09-30 10:48:01 +0100

    completed bonus exercise

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-const cards = [
+let cards = [
 {
 	rank: "queen",
 	suit: "hearts",
@@ -20,25 +20,79 @@ const cards = [
 	cardImage: "images/king-of-diamonds.png"
 }];
 
-const cardsInPlay = [];
+
+// use a Fisher-Yates shuffle
+
+function shuffle(array) {
+	var m = array.length, t, i;
+
+	// While there remain elements to shuffle...
+	while (m) {
+
+		// Pick a remaining element...
+		i = Math.floor(Math.random() * m--);
+
+		// And swap it with the current element.
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+
+	return array;
+}
+
+let cardsInPlay = null;
+let score = 0;
+let roundsPlayed = 0;
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		score += 10;
 } 	else {
   		alert("Sorry, try again.");
 }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	if (cardsInPlay.length > 1) {
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	cards = shuffle(cards);
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	cardsInPlay = [];
+}
+
+createBoard();
+
+function keepScore() {
+	document.getElementById('score').innerHTML = score;
+}
+	
+keepScore();
+
+function restartGame() {
+	// why does removeChild() not lead to same outcome as innerHTML below?
+	document.getElementById('game-board').innerHTML = "";
+		createBoard();
+		roundsPlayed += 1;
+		document.getElementById('round').innerHTML = roundsPlayed;
+		keepScore();
+	}
+
+document.querySelector('button').addEventListener('click', restartGame);
